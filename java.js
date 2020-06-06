a3135ef68eb0ffc1e1d9c7a36dbf3e3dcb43af70
Rakib-Hassan <akonabdullah.cse@gmail.com>
2020-06-06 14:06:45 +0600

    this code is branchA code

--- java.js
+++ java.js
@@ -5,3 +5,8 @@ function hello() {
 function hello2() {
     console.log('hello rakib you better know to me')
 }
+
+var arr = [2, 4, 5, 8, 9]
+for (var i = 0; i < arr; i++){
+    console.log('hey mr tembourin man ,a play song for me')
+}
