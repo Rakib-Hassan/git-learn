a9ef6fb5d4b47cb9765ba2ce24e703b866ddbfdb
Rakib-Hassan <akonabdullah.cse@gmail.com>
2020-06-06 16:27:31 +0600

    this commit change is from another branch

--- java.js
+++ java.js
@@ -2,11 +2,8 @@ function hello() {
     console.log('hello Rakib how are you')
 }
 
-function hello2() {
-    console.log('hello rakib you better know to me')
-}
 
-var arr = [2, 4, 5, 8, 9]
-for (var i = 0; i < arr; i++){
-    console.log('hey mr tembourin man ,a play song for me')
+
+function anathoerbrnc(){
+    console.log('this is from another branch')
 }
